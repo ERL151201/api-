578f945cc2a4f43b25bfd3d790b7377f0d41b201
Emanuel Rangel <emanuelrangelluna@gmail.com>
2021-04-16 12:33:19 -0500

    7_1 Desplegar nuestra API

---
+++ src/main/java/com/emanuel/market/persistence/crud/ProductoCrudRepository.java
@@ -0,0 +1,14 @@
+package com.emanuel.market.persistence.crud;
+
+import com.emanuel.market.persistence.entity.Producto;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ProductoCrudRepository  extends CrudRepository<Producto, Integer> {
+
+    List<Producto> findByIdCategoriaOrderByNombreAsc(int idCategoria);
+
+    Optional<List<Producto>> findByCantidadStockLessThanAndEstado(int cantidadStock, boolean estado);
+}
